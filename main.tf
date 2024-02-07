ef7e23f2948f9ade092c8eeeb5c68f0a38a0dbf4
1ShaikhAsif <142223292+1ShaikhAsif@users.noreply.github.com>
2024-02-07 18:17:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIAWHA7ZTBFYSLEV2E4"
-secret_key = "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"
+access_key = ""
+secret_key = ""
 }
 
 resource "aws_instance" "one" {
